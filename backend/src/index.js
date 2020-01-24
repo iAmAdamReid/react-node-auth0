d2ea8dc6f0c3181a18ba073a2f059803c4399e1d
iAmAdamReid <adam@adamreid.me>
2020-01-23 17:59:27 -0800

    initialize express and import cors middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(4000, () => console.log(`Application listening on port 4000.`));
